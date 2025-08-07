906bc0a2f138f91e381c4b71843916bf403c6976
Lurian Oriane <lurian.oriane@ifood.com.br>
2025-08-07 09:07:24 -0300

    Updating kotlin and ksp version

--- build-logic/convention/src/main/java/config/AndroidCompose.kt
+++ build-logic/convention/src/main/java/config/AndroidCompose.kt
@@ -9,72 +9,72 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 /**
-* Configure Compose-specific options
-*/
+ * Configure Compose-specific options
+ */
 internal fun Project.configureAndroidCompose(
-   commonExtension: CommonExtension<*, *, *, *, *, *>,
+    commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
-   commonExtension.apply {
-       buildFeatures {
-           compose = true
-       }
+    commonExtension.apply {
+        buildFeatures {
+            compose = true
+        }
 
 
-       composeOptions {
-           kotlinCompilerExtensionVersion =
-               libs.findVersion("androidxComposeCompiler").get().toString()
-       }
+        composeOptions {
+            kotlinCompilerExtensionVersion =
+                libs.findVersion("androidxComposeCompiler").get().toString()
+        }
 
 
-       dependencies {
-           val bom = libs.findLibrary("androidx-compose-bom").get()
-           add("implementation", platform(bom))
-           add("implementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
-           add("debugImplementation", libs.findLibrary("androidx-compose-ui-tooling").get())
-           add("implementation", libs.findLibrary("androidx-material3").get())
-           add("implementation", libs.findLibrary("androidx-foundation-android").get())
-           add("implementation", libs.findLibrary("androidx-ui").get())
-           add("implementation", libs.findLibrary("androidx-compose-ui-tooling").get())
-           add("implementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
-           add("implementation", libs.findLibrary("androidx-navigation-compose").get())
-           add("implementation", libs.findLibrary("androidx-hilt-navigation-compose").get())
-       }
-   }
+        dependencies {
+            val bom = libs.findLibrary("androidx-compose-bom").get()
+            add("implementation", platform(bom))
+            add("implementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
+            add("debugImplementation", libs.findLibrary("androidx-compose-ui-tooling").get())
+            add("implementation", libs.findLibrary("androidx-material3").get())
+            add("implementation", libs.findLibrary("androidx-foundation-android").get())
+            add("implementation", libs.findLibrary("androidx-ui").get())
+            add("implementation", libs.findLibrary("androidx-compose-ui-tooling").get())
+            add("implementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
+            add("implementation", libs.findLibrary("androidx-navigation-compose").get())
+            add("implementation", libs.findLibrary("androidx-hilt-navigation-compose").get())
+        }
+    }
 
 
-   tasks.withType<KotlinCompile>().configureEach {
-       kotlinOptions {
-           freeCompilerArgs += buildComposeMetricsParameters()
-       }
-   }
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            freeCompilerArgs.addAll(buildComposeMetricsParameters())
+        }
+    }
 }
 
 
 private fun Project.buildComposeMetricsParameters(): List<String> {
-   val metricParameters = mutableListOf<String>()
-   val enableMetricsProvider = project.providers.gradleProperty("enableComposeCompilerMetrics")
-   val relativePath = projectDir.relativeTo(rootDir)
-   val buildDir = layout.buildDirectory.get().asFile
-   val enableMetrics = (enableMetricsProvider.orNull == "true")
-   if (enableMetrics) {
-       val metricsFolder = buildDir.resolve("compose-metrics").resolve(relativePath)
-       metricParameters.add("-P")
-       metricParameters.add(
-           "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + metricsFolder.absolutePath,
-       )
-   }
+    val metricParameters = mutableListOf<String>()
+    val enableMetricsProvider = project.providers.gradleProperty("enableComposeCompilerMetrics")
+    val relativePath = projectDir.relativeTo(rootDir)
+    val buildDir = layout.buildDirectory.get().asFile
+    val enableMetrics = (enableMetricsProvider.orNull == "true")
+    if (enableMetrics) {
+        val metricsFolder = buildDir.resolve("compose-metrics").resolve(relativePath)
+        metricParameters.add("-P")
+        metricParameters.add(
+            "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" + metricsFolder.absolutePath,
+        )
+    }
 
 
-   val enableReportsProvider = project.providers.gradleProperty("enableComposeCompilerReports")
-   val enableReports = (enableReportsProvider.orNull == "true")
-   if (enableReports) {
-       val reportsFolder = buildDir.resolve("compose-reports").resolve(relativePath)
-       metricParameters.add("-P")
-       metricParameters.add(
-           "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + reportsFolder.absolutePath
-       )
-   }
+    val enableReportsProvider = project.providers.gradleProperty("enableComposeCompilerReports")
+    val enableReports = (enableReportsProvider.orNull == "true")
+    if (enableReports) {
+        val reportsFolder = buildDir.resolve("compose-reports").resolve(relativePath)
+        metricParameters.add("-P")
+        metricParameters.add(
+            "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=" + reportsFolder.absolutePath
+        )
+    }
 
 
-   return metricParameters.toList()
+    return metricParameters.toList()
 }
